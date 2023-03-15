f82b9a7522c96826a709f32ba2cf40170bc2198b
RyuNen344 <s1100633@outlook.com>
2023-03-16 08:17:02 +0900

    feat: impl archive to NSData

--- shared/src/iosMain/kotlin/io/github/ryunen344/kn/keyed/Book.kt
+++ shared/src/iosMain/kotlin/io/github/ryunen344/kn/keyed/Book.kt
@@ -9,8 +9,10 @@ import platform.Foundation.NSCoder
 import platform.Foundation.NSCodingProtocol
 import platform.Foundation.NSData
 import platform.Foundation.NSError
+import platform.Foundation.NSKeyedArchiver
 import platform.Foundation.NSKeyedUnarchiver
 import platform.Foundation.NSString
+import platform.Foundation.classForKeyedArchiver
 import platform.Foundation.classForKeyedUnarchiver
 import platform.Foundation.decodeObjectForKey
 import platform.Foundation.encodeObject
@@ -35,17 +37,37 @@ class Book(
     companion object : NSObjectMeta()
 }
 
+fun archiveBook(book: Book): NSData? {
+    return memScoped {
+        val archiveError = alloc<ObjCObjectVar<NSError?>>()
+        NSKeyedArchiver.setClassName(
+            codedName = "Book",
+            forClass = book.classForKeyedArchiver()!!
+        )
+        val archived = NSKeyedArchiver.archivedDataWithRootObject(
+            `object` = book,
+            requiringSecureCoding = false,
+            error = archiveError.ptr
+        )
+        archiveError.value?.let {
+            // NSErrorがnon nullのときはエラーが発生してる
+            error(it.localizedDescription)
+        }
+        archived
+    }
+}
+
 fun unarchiveBook(data: NSData): Book? {
     return memScoped {
         val unarchiveError = alloc<ObjCObjectVar<NSError?>>()
         NSKeyedUnarchiver.setClass(
-            Book.classForKeyedUnarchiver(),
-            "Book"
+            cls = Book.classForKeyedUnarchiver(),
+            forClassName = "Book"
         )
         val unarchived = NSKeyedUnarchiver.unarchivedObjectOfClasses(
-            setOf(Book, NSString),
-            data,
-            unarchiveError.ptr
+            classes = setOf(Book, NSString),
+            fromData = data,
+            error = unarchiveError.ptr
         ) as? Book
         unarchiveError.value?.let {
             // NSErrorがnon nullのときはエラーが発生してる
